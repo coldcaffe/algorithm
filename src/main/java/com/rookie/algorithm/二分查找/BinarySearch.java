fe219de4b8bb8bf4c5fe32e620fb2eb8ec464386
Eric <358638369@qq.com>
2020-05-19 12:24:11 +0800

    first demo;

---
+++ src/main/java/com/rookie/algorithm/二分查找/BinarySearch.java
@@ -0,0 +1,13 @@
+package com.rookie.algorithm.二分查找;
+
+/**
+ * @Author: Eric
+ * @Date: 2020/5/19 12:23
+ * @Desc:
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        System.out.println("hello  world");
+    }
+}
